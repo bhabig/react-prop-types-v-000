51e7186777fb8127de75b8ae9b006b39e306c734
Bob Habig <rfhab2@gmail.com>
2018-02-07 02:31:12 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,46 +1,45 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom';
 
 class Order extends React.Component {
   render() {
     return (
-      <div className="order">
-        <ul>
-          <li>{this.props.cone ? 'Cone' : 'Cup'}</li>
-          <li>{this.props.size}</li>
-          <li>{this.props.scoops.length} scoops: {this.props.scoops.join(', ')}</li>
-          <li>Ordered by {this.props.orderInfo.customerName} at {this.props.orderInfo.orderedAt}.</li>
-        </ul>
+      <div>
+        <h1>Order For:</h1>
+        <h2>{this.props.orderInfo.customerName}</h2>
+        <h3>Flavors:</h3>
+        <ul>{
+          this.props.scoops.length > 0 ?
+            this.props.scoops.map(flav => <li>{flav}</li>) :
+            <p>You don't know how to order Ice Cream...</p>
+        }</ul>
+
+        <h3>{this.props.cone ? "Cone" : "Bowl"} [{this.props.size}]</h3>
+        <h4>Other Info: {this.props.orderInfo.toppings}</h4>
+        <p>Ordered On: {this.props.orderInfo.orderedAt}</p>
       </div>
-    );
+    )
   }
 }
 
 Order.defaultProps = {
   cone: true,
-  size: 'regular'
-};
+  size: 'Regular',
+}
 
 Order.propTypes = {
-  cone: PropTypes.bool,
-  size: PropTypes.string,
+  cone: PropTypes.bool.isRequired,
+  size: PropTypes.oneOf(['Small', 'Regular', 'Large', 'Extra-Large']),
   scoops: PropTypes.arrayOf(PropTypes.string).isRequired,
   orderInfo: PropTypes.shape({
     customerName: PropTypes.string.isRequired,
-    orderedAt: PropTypes.string.isRequired
-  }).isRequired
-};
-
-const order = { customerName: 'Voldemort', orderedAt: new Date().toString() }
+    toppings: PropTypes.string,
+    orderedAt: PropTypes.string,
+  })
+}
 
 ReactDOM.render(
-  <Order
-    cone={false}
-    size='large'
-    scoops={['Choc. Chip Cookie Dough', 'Banana Nut', 'Choc. Chip Cookie Dough']}
-    customerName="Voldemort"
-    orderInfo={ order }
-  />,
+  <Order cone={true} size="Extra-Large" scoops={['Vanilla', 'Cookie Dough', 'Vanilla', 'Cookie Dough']} orderInfo={{customerName: "Bob", toppings: "tons of whipped cream!!!", orderedAt: new Date(Date.now()).toLocaleString()}}/>,
   document.getElementById('root')
-);
+)
